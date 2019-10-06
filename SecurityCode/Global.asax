a87b18b55211699f310a2348ec4758f416ce5a35
ilgiz_t <tim.ilgiz@gmail.com>
2019-10-06 23:36:17 +0300

    start

---
+++ SecurityCode/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SecurityCode.MvcApplication" Language="C#" %>
---
+++ SecurityCode/Models/EditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityCode.Models
+{
+    public class EditViewModel
+    {
+        public Activities Activity { get; set; }
+        public SelectList Categories { get; set; }
+        public int SelectedParentActivityId { get; set; }
+    }
+}
